23288e99697bc629c1aadd311d388ca77b03b2f2
Thulibathi <nokuthulamargaret@gmail.com>
2023-05-12 12:31:48 +0200

    Section 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name : string | undefined;
+  date: string | undefined;
+  amount: number | undefined;
+  height: number | undefined;
+  miles: number;
 
-  
+  car = {
+    make: 'Toyota',
+    model:'Camry',
+    year: 2000
+  }
+
+  onMilesChange(value:string)
+  {
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(value:string)
+  {
+    this.height = parseFloat(value);
+  }
+
   onNameChange(value: string){
     this.name = value;
+  }
+
+  onDateChange(value:string)
+  {
+    this.date = value;
+  }
+
+  onAmountChange(value: string)
+  {
+    this.amount = parseFloat(value);
   }
 }
